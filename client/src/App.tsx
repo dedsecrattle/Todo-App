7cb471859ca23645c7c0176b194adef51ae01cdb
Prabhat Kumar <itsprabxxx@gmail.com>
2024-05-26 13:48:02 +0530

    Ui updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import useSWR, { mutate } from 'swr'
-import './App.css'
 import { Box ,  List,
   ListItem,
   ListIcon,
   OrderedList,
   Heading,
-  CheckboxIcon,} from '@chakra-ui/react';
+  CheckboxIcon,
+  Text,
+  Icon,} from '@chakra-ui/react';
+import { MdCheckBox, MdRadioButtonUnchecked } from 'react-icons/md';
 
 export const ENDPOINT = "http://localhost:4000";
 
@@ -22,24 +24,23 @@ const fetcher = (url: string) =>
 function App() {
   const {data, mutate} = useSWR<Todo[]>("todo", fetcher)
   return (
-    <Box>
-      <Heading>
+    <Box display= 'flex' flexDirection='column' justifyContent='center' alignItems='center' margin='1.5rem'>
+      <Heading marginBottom='1.5rem'>
         Todos
       </Heading>
-      <OrderedList>
-        {data?.map((todo) => {
-          return (
-            <ListItem>
-              {todo.done ?
-              (<ListIcon as={CheckboxIcon} color='green.500' />)
-              : (<ListIcon as={CheckboxIcon} color='red'/>)
-              }
-              
-              {todo.title}
-            </ListItem>
-          )
-        })}
-      </OrderedList>
+      <List>
+      {data?.map((todo) => {
+        return <ListItem border='1px' padding='1rem' borderRadius='10' margin='1rem'>
+          <Box display='flex' flexDirection='row' w='75%' gap='10' justifyContent='center' alignItems='center' textAlign='center'>
+          {todo.done ? <Icon boxSize={8} as={MdCheckBox} color='green.500'></Icon> 
+          : <Icon boxSize={8} as={MdRadioButtonUnchecked} color='red.500'></Icon> 
+          }
+          <Heading size='xl'>{todo.title}</Heading>
+          <Text fontSize='3xl'>{todo.body}</Text>
+          </Box>
+        </ListItem>
+      })}
+      </List>
     </Box>
   )
 }
