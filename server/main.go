ee4175befa5c43e4c2a38574a94fc7a8bdfb1cf7
Prabhat Kumar <itsprabxxx@gmail.com>
2024-05-27 22:21:57 +0530

    Connection with Database

--- server/main.go
+++ server/main.go
@@ -1,14 +1,51 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/dedsecrattle/todo-application/models"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var Todos []models.Todo = []models.Todo{}
 
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("Set your 'MONGODB_URI' environment variable. " +
+			"See: " +
+			"www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().
+		ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Err(); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
 	app := fiber.New()
 
 	app.Use(cors.New())
@@ -45,5 +82,20 @@ func main() {
 
 		return c.JSON(Todos)
 	})
+
 	app.Listen(":4000")
+}
+
+func CreateTodo(title, body string, todoCollection mongo.Collection) (*models.Todo, error) {
+	newTodo := models.Todo{
+		Title: title,
+		Body:  body,
+		Done:  false,
+	}
+	result, err := todoCollection.InsertOne(context.TODO(), newTodo)
+	if err != nil {
+		return nil, err
+	}
+	newTodo.ID = result.InsertedID.(primitive.ObjectID).Hex()
+	return &newTodo, nil
 }
--- server/models/todo.go
+++ server/models/todo.go
@@ -1,10 +1,8 @@
 package models
 
 type Todo struct {
-	ID    int    `json:"id"`
-	Title string `json:"title"`
-	Body  string `json:"body"`
-	Done  bool   `json:"done"`
+	ID    int    `json:"id" bson:"_id"`
+	Title string `json:"title" bson:"title"`
+	Body  string `json:"body" bson:"body"`
+	Done  bool   `json:"done" bson:"done"`
 }
-
-
